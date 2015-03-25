60c09bd709b259df435fa2a4ae5451fa78e1762c
Anne <anne@tagtoo.org>
2015-03-26 01:01:52 +0800

    The big main.py

---
+++ hdr.py
@@ -0,0 +1,24 @@
+import cv
+import cv2
+import sys
+from MTB import *
+from curve import *
+
+#------ Main function
+# argv = image path, num_frame, ref_frame, level
+if( len(sys.argv) != 5 ) :
+    print 'hdr <image_path> <num_frame> <ref_frame> <level>'
+    quit()
+
+image_path = sys.argv[1]    
+num_frame = int(sys.argv[2])
+ref_frame = int(sys.argv[3])
+level = int(level)
+if ref_frame >= num_frame:
+    print 'invalid reference frame index!'
+    quit()
+level = int(sys.argv[4])
+result = solveCurve(str(image_path))
+#result = numpy.zeros((256, 1))
+radianceMap(str(image_path), result)
+align(num_frame, ref_frame, level)
